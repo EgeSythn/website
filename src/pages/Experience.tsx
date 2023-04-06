2d8773b04d2e47efa73c2de50cc046f20c190ab1
EgeSythn <eseyith1@gmail.com>
2023-04-06 14:33:37 -0400

    In progress image

--- src/pages/Experience.tsx
+++ src/pages/Experience.tsx
@@ -1,5 +1,14 @@
-import { Container, Title, Divider, useMantineTheme } from "@mantine/core";
+import {
+  Container,
+  Title,
+  Divider,
+  useMantineTheme,
+  Image,
+  Stack,
+  Text,
+} from "@mantine/core";
 import ExperienceCard from "./pages-components/ExperienceCard";
+import hard_hat from "../assets/hard_hat.png";
 
 function Experience() {
   const theme = useMantineTheme();
@@ -15,6 +24,20 @@ function Experience() {
             : theme.colors.blue[4]
         }
       />
+      <Stack style={{ paddingTop: "2.5%" }} align="center">
+        <Image src={hard_hat} maw={240} />
+        <Text size={25} weight={700}>
+          Page Under Construction
+        </Text>
+        <Text size={10}>
+          <a
+            href="https://www.flaticon.com/free-icons/work-in-progress"
+            title="work in progress icons"
+          >
+            Work in progress icons created by Freepik - Flaticon
+          </a>
+        </Text>
+      </Stack>
     </Container>
   );
 }
