5f37a485b016b5a9fc861621080c24ed280670cf
EgeSythn <eseyith1@gmail.com>
2023-05-19 19:13:06 -0400

    Some curves

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -121,9 +121,9 @@ function Home() {
       <div className={classes.section}>
         <TypeAnimation
           sequence={[
-            "Hi, I'm a student!",
+            "I'm a student!",
             1000,
-            "Hi, I'm an engineer!",
+            "I'm an engineer!",
             1000,
             "Hi, I'm Ege!",
           ]}
