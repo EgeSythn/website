16cdc6c2be7f1415612d1e613acd7d9879988099
EgeSythn <eseyith1@gmail.com>
2023-06-04 00:52:04 -0400

    Map loading indicated

--- src/pages/pages-components/Map.tsx
+++ src/pages/pages-components/Map.tsx
@@ -1,20 +1,53 @@
-import { Center, Container } from "@mantine/core";
+import { Center, Container, Loader } from "@mantine/core";
+import { useState } from "react";
 
 function Map(props: { embedLink: string }) {
   const { embedLink } = props;
+  const [isLoading, setIsLoading] = useState(true);
+
+  const iframeContainerStyle: React.CSSProperties = {
+    position: "relative",
+    width: "300px",
+    height: "300px",
+  };
+
+  const loaderStyle: React.CSSProperties = {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    zIndex: 1,
+  };
+
   return (
     <Container style={{ paddingTop: "2.5%" }}>
       <Center>
-        <iframe
-          title="map"
-          src={embedLink}
-          width="300"
-          height="300"
-          style={{ border: 0, borderRadius: "20px" }}
-          allowFullScreen={false}
-          loading="lazy"
-          referrerPolicy="no-referrer-when-downgrade"
-        />
+        <div style={iframeContainerStyle}>
+          {isLoading && (
+            <div style={loaderStyle}>
+              <Loader />
+            </div>
+          )}
+          <iframe
+            title="map"
+            src={embedLink}
+            width="300"
+            height="300"
+            style={{
+              backgroundColor: "#F8F9F9",
+              border: 0,
+              borderRadius: "20px",
+            }}
+            allowFullScreen={false}
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+            onLoad={() => setIsLoading(false)}
+          />
+        </div>
       </Center>
     </Container>
   );
