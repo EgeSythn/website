db725f078e92fa7a229c51073a811a0fc1d620b6
EgeSythn <eseyith1@gmail.com>
2023-06-08 13:28:16 -0400

    Bonus page adjusted

--- src/pages/Bonus.tsx
+++ src/pages/Bonus.tsx
@@ -13,7 +13,9 @@ import {
 } from "@mantine/core";
 import { IconExternalLink } from "@tabler/icons-react";
 import spaceinvader from "../assets/spaceinvader.png";
+import flying_saucer from "../assets/flying_saucer.gif";
 import { gameUrl } from "../assets/data";
+import { useMediaQuery } from "@mantine/hooks";
 
 function Bonus() {
   const theme = useMantineTheme();
@@ -34,55 +36,49 @@ function Bonus() {
           style={{
             paddingBottom: "2.5%",
           }}
+          ta="center"
           size={10}
           fw={700}
         >
-          This is a place to share some fun and small scale personal endeavors
-          with you. If the page looks a bit barren, don't worry, as there is
-          more content in the works!
+          This is a place to share some fun little personal endeavors with you.
+          If the page looks a bit barren, don't worry, as there is more content
+          in the works!
         </Text>
       </Center>
       <Card shadow="sm" padding="lg" radius="md" withBorder>
         <Flex direction="column" align="stretch">
-          <Center>
-            <Text
-              style={{
-                paddingTop: "2.5%",
-                paddingBottom: "2.5%",
-              }}
-              size={25}
-              fw={700}
-            >
-              🛸Space Invaders🛸
+          <Flex direction="row" style={{ paddingBottom: "2.5%" }}>
+            <Text style={{}} size={25} fw={700}>
+              &nbsp;Space Invaders&nbsp;
             </Text>
-          </Center>
-          <Image src={spaceinvader} fit="cover" height="20%" radius={10} />
-          <Stack spacing="xs" style={{ paddingTop: "5%" }}>
-            <Text fw={400} style={{ paddingBottom: "0.5%" }}>
-              &emsp;&emsp;You can play Space Invaders here! This is nothing
-              special, but as one of the first things I programmed while
-              learning web development (like many other developers), I wanted to
-              share it with you.{" "}
-            </Text>
-            <Text fw={400} style={{ paddingBottom: "0.5%" }}>
-              &emsp;&emsp;Unfortunately the game is not compatible with mobile
-              devices, but if you're on a computer, give it a go and enjoy!
-            </Text>
-            <Center>
-              <Text fw={400} style={{ paddingBottom: "2.5%" }}>
-                Good luck brave explorer 🚀
+            <Image src={flying_saucer} maw="5%" radius={10} />
+          </Flex>
+          <Flex direction="row">
+            {!useMediaQuery("(max-width: 600px)") && (
+              <Image src={spaceinvader} maw="40%" radius={10} />
+            )}
+            <Stack spacing="xs" style={{ paddingLeft: "2.5%" }}>
+              <Text fw={400} style={{ paddingBottom: "0.5%" }}>
+                &emsp;&emsp;Nothing special, but as one of the first things I
+                programmed while learning web development (like many other
+                developers), I wanted to share it with you.{" "}
               </Text>
-            </Center>
+              <Center>
+                <Text fw={400} style={{ paddingBottom: "2.5%" }}>
+                  Good luck brave explorer 🚀
+                </Text>
+              </Center>
 
-            <Button
-              variant="outline"
-              radius="xl"
-              rightIcon={<IconExternalLink size={15} />}
-              onClick={() => window.open(gameUrl, "_blank")}
-            >
-              Play
-            </Button>
-          </Stack>
+              <Button
+                variant="outline"
+                radius="xl"
+                rightIcon={<IconExternalLink size={15} />}
+                onClick={() => window.open(gameUrl, "_blank")}
+              >
+                Play
+              </Button>
+            </Stack>
+          </Flex>
         </Flex>
       </Card>
     </Container>
